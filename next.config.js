f0c5eb120ed7322452f36453d9c91b34d3b84a27
orangeorangehuang <orange1052110128@gmail.com>
2023-05-31 18:54:05 +0800

    fix: edit next config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  experimental: {
-    serverComponentsExternalPackages: ['mongoose'],
-  },
 };
 
 module.exports = nextConfig;
